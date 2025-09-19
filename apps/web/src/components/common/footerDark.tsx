990951658fb082642ab00780ef72b6e6ef41bc0e
Devika <dileepdevika3@gmail.com>
2025-09-19 16:49:28 +0530

    fix: update footer links to point to the correct GitHub repository

--- apps/web/src/components/common/footerDark.tsx
+++ apps/web/src/components/common/footerDark.tsx
@@ -5,11 +5,11 @@ export function FooterDark() {
   return (
     <footer className="relative w-full shrink-0 mt-[5vh] md:mt-[-10vh] flex flex-col">
       {/* Background container with proper black background for mobile */}
-      <div className="absolute inset-x-0 md:bottom-0 bottom-0 w-full h-full md:h-[100vh] bg-black  overflow-hidden z-0">
+      <div className="absolute inset-x-0 md:bottom-0 bottom-0 w-full h-full md:h-[100vh] bg-black md:bg-transparent overflow-hidden z-0">
         <div className="w-full absolute h-full md:bg-[url('/hero-img.webp')] bg-[url('/img-mob.svg')] bg-no-repeat bg-contain rotate-180"/>
       </div>
       
-      <div className="flex flex-col z-10 w-full flex-grow bg-black">
+      <div className="flex flex-col z-10 w-full flex-grow bg-black md:bg-transparent">
         <div className="z-10 w-full md:flex md:items-end h-auto md:h-[100vh] mx-auto px-6 md:px-6 py-8">
           <div className="flex md:hidden flex-col items-start gap-3 mb-8">
             <Link
@@ -63,16 +63,16 @@ export function FooterDark() {
             <FooterLinksGroup
               title="Resources"
               links={[
-                { href: "#", label: "Contribution Guide" },
-                { href: "#", label: "Code of Conduct" },
-                { href: "#", label: "License" },
+                { href: "https://github.com/The-Purple-Movement/Beyond-Syllabus", label: "Contribution Guide" },
+                { href: "https://github.com/The-Purple-Movement/Beyond-Syllabus", label: "Code of Conduct" },
+                { href: "https://github.com/The-Purple-Movement/Beyond-Syllabus", label: "License" },
               ]}
             />
             <FooterLinksGroup
               title="Legal"
               links={[
-                { href: "#", label: "Terms of Service" },
-                { href: "#", label: "Privacy Policy" },
+                { href: "https://github.com/The-Purple-Movement/Beyond-Syllabus", label: "Terms of Service" },
+                { href: "https://github.com/The-Purple-Movement/Beyond-Syllabus", label: "Privacy Policy" },
               ]}
             />
           </div>
